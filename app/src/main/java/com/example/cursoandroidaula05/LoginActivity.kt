6dc8526b2e7930c202b42898938bb2f326821ef6
Gladys María Toledo González <gladysmtg1999@gmail.com>
2022-08-24 17:05:13 -0400

    Setting viewBinding and adding the strings files in portuguese and spanish

--- app/src/main/java/com/example/cursoandroidaula05/LoginActivity.kt
+++ app/src/main/java/com/example/cursoandroidaula05/LoginActivity.kt
@@ -1,11 +1,18 @@
 package com.example.cursoandroidaula05
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cursoandroidaula05.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
