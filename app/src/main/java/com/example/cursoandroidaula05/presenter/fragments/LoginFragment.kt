c03109d7dc78221f9ea6af2216094d53c7031275
Gladys María Toledo González <gladysmtg1999@gmail.com>
2022-09-05 10:57:52 -0400

    Setting navigation in gradle

--- app/src/main/java/com/example/cursoandroidaula05/presenter/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/cursoandroidaula05/presenter/fragments/LoginFragment.kt
@@ -1,7 +1,6 @@
 package com.example.cursoandroidaula05.presenter.fragments
 
 import android.os.Bundle
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
